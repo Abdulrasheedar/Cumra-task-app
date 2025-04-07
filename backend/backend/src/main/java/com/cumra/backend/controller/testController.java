dc708e4d3b9acd8be2e4d2862652d00c12dcd9e0
Abdulrasheedar <abdulrasheed.razaak@gmail.com>
2025-04-07 16:20:04 +0530

    Spring Boot + MySQL initial setup with configs

---
+++ backend/backend/src/main/java/com/cumra/backend/controller/testController.java
@@ -0,0 +1,12 @@
+package com.cumra.backend.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class testController {
+	@GetMapping("/")
+    public String home() {
+        return "Welcome to the Home Page!";
+    }
+}
